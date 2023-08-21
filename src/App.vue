fd1ff3c23989d4b5e5c762b2bb60b35037ab49df
Markelas <star.wars.bat@yandex.ru>
2023-08-22 01:24:43 +0500

    Add new files and finished project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
+  <div class="container with-nav">
+    <div class="navbar">
+      <h2>Погода</h2>
+    </div>
+    <div class="card">
+      <h2>Погода в РФ</h2>
+      <select v-model="selected" class="form-control">
+        <option disabled value="">Выберите название города</option>
+        <option v-for="item in cityList" :key="item">{{ item }}</option>
+      </select>
+      <button class="btn primary" @click="load">
+        Узнать погоду в {{ selected }}
+      </button>
+      <div>
+        <h3>
+          Погода в городе {{ selected }} сейчас:
+          {{ this.$store.state.cityTemp }}
+          градусов цельсия
+        </h3>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      selected: "",
+      cityList: this.$store.state.city,
+    };
+  },
+  computed: {
+    load() {
+      return this.$store.dispatch("loadWeather", this.selected);
+    },
+  },
+  methods: {},
+});
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,47 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    city: [
+      "Kazan",
+      "Moscow",
+      "Barnaul",
+      "Neftekamsk",
+      "Ufa",
+      "Saint Petersburg",
+      "Irkutsk",
+      "Omsk",
+      "Magadan",
+      "Novorossiysk",
+    ],
+    activeCity: "",
+    cityTemp: "",
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    updTemp(state, context) {
+      state.cityTemp = context;
+    },
+  },
+  actions: {
+    async loadWeather(context, payload) {
+      //Передаём выбранный город
+      console.log(payload);
+      try {
+        const { data } = await axios.get(
+          `http://api.weatherapi.com/v1/current.json?key=7826d28b54cf46c3ba2174336232108&q=${payload}`
+        );
+        const temp = data.current.temp_c; //Получаем температуру
+        console.log(temp);
+        context.commit("updTemp", temp); //Передаём значение температуры в мутации, для изменения state
+      } catch (e: any) {
+        console.log(e.message);
+      }
+    },
+  },
   modules: {},
 });
